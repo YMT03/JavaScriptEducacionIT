71d5f9eb87022dfd7d4affa71ee865e7fc5fc524
YMT03 <nachito.eze.ramos@gmail.com>
2019-04-01 12:50:56 -0300

    Primera Clase

--- Clase 1/script.js
+++ Clase 1/script.js
@@ -23,7 +23,7 @@
  // Pasar por referencia
  var c = {x:1}
  var d = c
- d.x = 20;
+ d.x = 20
  
 //Template String, ventajas de ``
 
@@ -53,11 +53,11 @@ var interpolacionVar = `<head>
  *  Var - no tienen scope (salvo en functions)
  */
 
-var a = 1;
-var a = 10;
-a = 50;
+var a = 1
+var a = 10
+a = 50
 if(true){
-    var j = 9;
+    var j = 9
 }
 //Sigue existiendo j, es global.
 
@@ -67,12 +67,12 @@ if(true){
  *  Let - Si tienen scopes de bloque
  */
 
-let c = 15;
+let i = 15
 //let c = 14; (No se puede)
-c = 16;
+i = 16
 
 if(true){
-    let cc = 20;
+    let cc = 20
 }
 //No se puede acceder a cc fuera del bloque if
 
@@ -85,12 +85,12 @@ if(true){
  *  .. por lo que pierde un poco su poder de "constante"
  */
 
- const constante = 15;
+ const constante = 15
  // const constante = 10; (No se puede)
  // constante = 30; (No se puede)
 
  const ccc = {x:1}
- ccc.x = 20;
+ ccc.x = 20
 
  /** 
   * typeof retorna el tipo de dato.. medio pelo. typeof null retorna object por ej
@@ -153,3 +153,80 @@ if(true){
   * generan una secuencia en el navegador que vuelven a disparar las etapas de layout y paint
   * h1.innerText = "Lorem ipsum"
   */
+
+
+  //Crear elementos nuevos de DOM
+  let p = document.createElement("p")
+  p.innerText = "Lorem ipsum"
+  //Ya esta instanciado pero no esta en el DOM aun
+
+  // Agregar elementos al DOM
+  // Target.appendChild(Nodo)
+  let body = document.querySelector("body");
+  body.appendChild(p); //Siempre va a ir al final de ese elemento
+  //Target.insertBefore(NuevoNodo,NodoReferencia)
+  //Remover elementos
+  //body.removeChild(p); 
+
+  let div = document.querySelector("div")
+  //div.appendChild(p) Los elementos no se copian de lugar sino que se mueven por refencia en el DOM
+  // O sea que 'p' quedaria solo agregado a div y no a body.
+
+  //Clonar un elemento
+  let copia = p.cloneNode(true)
+  div.appendChild(copia)
+
+
+  let ul = document.querySelector("ul")
+/*
+  for (let i = 0; i < 5; i++) {
+      let li = document.createElement("li")
+      li.innerText = `Item ${i}`
+      ul.append(li)      
+  }
+*/
+  // Poco perfomante. Cada vez que se agrega un item al DOM se activa el relayout y repainting
+  
+  // DocumentFragment Es como una etiqueta HTML. Los puedo agrupar todos en èl y despues hacer el append al DOM.
+
+let f = document.createDocumentFragment()
+for (let i = 0; i < 5; i++) {
+    let li = document.createElement("li")
+    li.innerText = `Item ${i}`
+    f.append(li)      
+}
+
+ul.appendChild(f) //Ahora es solo una modificacion de DOM en vez de 5. Es mas performante.
+
+// Una vez que se usa appendChild f queda vacio de nuevo. Se puede reutilizar el mismo fragmento
+
+
+//Eventos
+
+let boton = document.querySelector("button")
+//boton.onclick = sayHi; Forma vieja, muy utilizada, no correcta
+//boton.onclick = sayBye; Reemplaza al anterior
+
+function sayHi(){
+    console.log("Hi")
+}
+function sayBye(){
+    console.log("Bye")
+}
+
+
+//Forma correcta, permite agregar varios listeners
+boton.addEventListener("click", sayHi)
+boton.addEventListener("click", sayBye)
+
+//Con funcion anonima
+boton.addEventListener("click", function(){
+    console.log("A")
+})
+
+//Funcion anonima a funcion lambda
+// function(){} === ()=>{}
+
+boton.addEventListener("click",()=>{
+    console.log("Lambda Funct")
+})
